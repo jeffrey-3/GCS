ba476bf485dfae96fd9bbce231dea311ad4c6122
JeffreyZhuang <jeffreyzhuangyt@gmail.com>
2024-11-29 17:47:01 -0500

    e

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+# Plan coordinates in google maps, then import or type the coordinates into GCS
+# Use PyQt drawing API to draw HUD on opencv pixmap
+# You can test using flightgear
 
-app = Flask(__name__)
+import pyqtgraph as pg
+from PyQt5 import QtWidgets
+from PyQt5.QtGui import *
+import numpy as np
+import cv2
 
-@app.route("/")
-def home():
-	return render_template('index.html')
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-if __name__ == "__main__":
-    app.run()
+        self.plot_graph = pg.PlotWidget()
+        self.setCentralWidget(self.plot_graph)
+        self.plot_graph.plot([0, 20, 100], [0, 50, -60], pen=pg.mkPen('r', width=5))
+        self.plot_graph.setXRange(-100, 100)
+        self.plot_graph.setYRange(-100, 100)
+        self.plot_graph.getPlotItem().hideAxis('bottom')
+        self.plot_graph.getPlotItem().hideAxis('left')
+        # self.plot_graph.getAxis('bottom').setTicks()
+        self.plot_graph.setAspectLocked(True)
+        arrow = pg.ArrowItem(angle=0, headLen=40, tipAngle=45, baseAngle=30, pen=QColor(255, 0, 0), brush=QColor(255, 0, 0))
+        self.plot_graph.addItem(arrow)
+
+        img = pg.ImageItem(cv2.cvtColor(cv2.imread("map.png"), cv2.COLOR_BGR2RGB))
+        img.scale(0.5, 0.5)
+        img.setZValue(-100)
+        self.plot_graph.addItem(img)
+
+app = QtWidgets.QApplication([])
+main = MainWindow()
+main.showFullScreen()
+app.exec()
